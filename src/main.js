b319283603fd4edfb1e9591680f17fab9d4a13da
Félix Riffont <igele2000@gmail.com>
2023-11-05 16:15:31 +0100

    ✨ texture on shader + camera movement

--- src/main.js
+++ src/main.js
@@ -2,8 +2,8 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import * as dat from 'lil-gui';
 
-// import vertexShader from './shaders/vertex.glsl';
-// import fragmentShader from './shaders/fragment.glsl';
+import vertexShader from './shaders/vertex.glsl';
+import fragmentShader from './shaders/fragment.glsl';
 
 export default class Sketch {
 	constructor() {
@@ -21,9 +21,13 @@ export default class Sketch {
 		});
 		this.renderer.setSize(this.sizes.width, this.sizes.height);
 		this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+		this.utils = {
+			rand: (min, max) => Math.random() * (max - min) + min,
+		};
 
 		// Init scene
 		this.scene = new THREE.Scene();
+		this.scene.background = new THREE.Color('#7FB2F0');
 
 		this.addLoader();
 
@@ -48,6 +52,7 @@ export default class Sketch {
 	addLoader() {
 		this.textureLoader = new THREE.TextureLoader();
 		this.cloudsTexture = this.textureLoader.load('/images/cloud.png');
+		this.clouds2Texture = this.textureLoader.load('/images/cloud_2.png');
 	}
 
 	addControls() {
@@ -60,38 +65,52 @@ export default class Sketch {
 			70,
 			this.sizes.width / this.sizes.height,
 			0.01,
-			10
+			20
 		);
-		this.camera.position.z = 1;
+		this.camera.position.z = -1;
+		this.camera.lookAt(new THREE.Vector3(0, 0, 0));
 	}
 
 	addMesh() {
-		this.geometry = new THREE.BufferGeometry();
-		this.count = 200;
+		this.fog = new THREE.Fog(0x4584b4, -100, 3000);
+		this.scene.fog = this.fog;
 
-		const positions = new Float32Array(this.count * 3);
+		this.plane = new THREE.PlaneGeometry(1, 1);
+
+		// this.material = new THREE.MeshBasicMaterial({
+		// 	side: THREE.DoubleSide,
+		// 	transparent: true,
+		// 	opacity: 0.9,
+		// 	alphaMap: this.cloudsTexture,
+		// 	depthTest: false,
+		// 	depthWrite: false,
+		// 	alphaTest: 0.01,
+		// });
+
+		this.material = new THREE.ShaderMaterial({
+			vertexShader: vertexShader,
+			fragmentShader: fragmentShader,
+			transparent: true,
+			depthTest: false,
+			depthWrite: false,
+			uniforms: {
+				uTexture: { value: this.cloudsTexture },
+			},
+			side: THREE.DoubleSide,
+		});
+
+		this.count = 4000;
 
 		for (let i = 0; i < this.count; i++) {
-			const i3 = i * 3;
-			positions[i3 + 0] = Math.random() - 0.5;
-			positions[i3 + 1] = Math.random() - 0.25;
-			positions[i3 + 2] = Math.random() - 0.5;
+			this.mesh = new THREE.Mesh(this.plane, this.material);
+			this.mesh.position.x = Math.random() * 20 - 10;
+			this.mesh.position.y = this.utils.rand(-1.5, -0.8);
+			this.mesh.position.z = this.utils.rand(-1, 100);
+			this.mesh.rotation.z = Math.random() * Math.PI;
+			this.mesh.scale.x = this.mesh.scale.y =
+				Math.random() * Math.random() * 1.5 + 0.5;
+			this.scene.add(this.mesh);
 		}
-
-		this.geometry.setAttribute(
-			'position',
-			new THREE.BufferAttribute(positions, 3)
-		);
-
-		this.material = new THREE.PointsMaterial();
-		this.material.size = 100;
-		this.material.sizeAttenuation = true;
-		this.material.alphaMap = this.cloudsTexture;
-		this.material.transparent = true;
-		this.material.depthTest = false;
-
-		this.mesh = new THREE.Points(this.geometry, this.material);
-		this.scene.add(this.mesh);
 	}
 
 	addDebug() {
@@ -100,6 +119,9 @@ export default class Sketch {
 
 	addAnim() {
 		const elapsedTime = this.clock.getElapsedTime();
+
+		this.camera.position.z = -(this.camera.position.z - elapsedTime) * 0.5;
+		console.log(this.camera.position.z);
 	}
 
 	resize() {
